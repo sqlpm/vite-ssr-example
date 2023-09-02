765f6c03e008c184a7771f13bf80b4982d77eefb
Eric Hosick <erichosick@gmail.com>
2023-09-01 20:17:30 -0700

    fix base of website

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/vite-ssr-example/',
   plugins: [react()],
   build: {
     minify: false,
