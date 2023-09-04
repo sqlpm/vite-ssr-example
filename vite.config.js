f6f73c62128a8b2ff6e459d2de34ac5c03260362
Eric Hosick <erichosick@gmail.com>
2023-09-04 11:25:56 -0700

    does what?

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/vite-ssr-example/',
   plugins: [react()],
   build: {
     minify: false,
